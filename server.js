d33f26fba55c68318a293ae3550c07a4ee8be869
Lorreina <slore@wood-btw-worlds>
2023-01-30 14:35:41 -0800

    install bootstrap

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const sequelize = require("./config/connection.js");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
-const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;

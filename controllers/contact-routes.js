257179d53e3904675f43047cebd8c403e0759e28
Lorreina <slore@wood-btw-worlds>
2023-02-05 15:43:19 -0800

    contact page created and connected

--- controllers/contact-routes.js
+++ controllers/contact-routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("contact");
 });
-
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const contactRoutes = require("./contact-routes");
 
 router.use("/api", apiRoutes);
 
 router.use("/", homeRoutes);
+router.use("/contact", contactRoutes);
+
 router.use((req, res) => {
   res.status(404).end();
 });

afbfb32678a702ae7fd89010b8ebbc1e0103431d
Lorreina <slore@wood-btw-worlds>
2023-02-07 11:38:13 -0800

    connect submit form to the backend

--- controllers/contact-routes.js
+++ controllers/contact-routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
+const { Queue } = require("../models");
 const sequelize = require("../config/connection");
 
 router.get("/", (req, res) => {
   res.render("contact");
 });
+
 module.exports = router;
---
+++ public/js/contact.js
@@ -0,0 +1,31 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector("#art_title").value.trim();
+  const author = document.querySelector("#art_author").value.trim();
+  const article_url = document.querySelector("#article_url").value.trim();
+  const policy = document.querySelector("#policy_cat").value.trim();
+  const summary = document.querySelector("#summary-message").value.trim();
+  const email = document.querySelector("#contact_email").value.trim();
+
+  if (title && author && article_url && email) {
+    fetch(`/api/queue`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        author,
+        article_url,
+        policy,
+        summary,
+        email,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+}
+
+document
+  .querySelector(".new-article-form")
+  .addEventListener("submit", newFormHandler);

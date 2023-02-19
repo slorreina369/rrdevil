9d7ebb297e416c01b098add98a7c77382abb05e5
Lorreina <slore@wood-btw-worlds>
2023-02-18 22:28:56 -0800

    implimented queue confirm

--- public/js/confirm-btn.js
+++ public/js/confirm-btn.js
@@ -1,23 +1,42 @@
 async function confirmFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector(".title").value.trim();
-  const author = document.querySelector(".author").value.trim();
-  const article_url = document.querySelector(".art_url").value.trim();
-  const summary = document.querySelector("./summary").value.trim();
-  const policy = document.querySelector("./policy-choice").value.trim();
+  const form = event.target.closest("form");
 
-  const response = await fetch(`/api/articles`, {
-    method: "POST",
-    body: JSON.stringify({
-      title,
-      author,
-      article_url,
-      summary,
-      policy,
-    }),
-    headers: { "Content-Type": "application/json" },
-  }).then((response) => {
-    console.log(response);
-  });
+  const title = form.querySelector(".title").innerText.trim();
+  const author = form.querySelector(".author").innerText.trim();
+  const article_url = form.querySelector(".art_url").innerText.trim();
+  const summary = form.querySelector(".summary").innerText.trim();
+
+  const policy_id = form.querySelector(".policy-choice").value.trim();
+  const submission_id = form.querySelector(".submission-id").value.trim();
+
+  if (policy_id) {
+    fetch(`/api/articles`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        author,
+        article_url,
+        summary,
+        policy_id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .then(() => {
+        return fetch(`api/queue/${submission_id}`, {
+          method: "DELETE",
+        });
+      })
+      .then(() => {
+        location.reload();
+      });
+  }
 }
+
+document.querySelectorAll(".accept-form").forEach((el) => {
+  el.addEventListener("submit", confirmFormHandler);
+});

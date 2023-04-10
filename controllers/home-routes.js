d662d5c4178aada1d5a751b3218828c00fcc5b88
Lorreina <slore@wood-btw-worlds>
2023-04-10 15:32:22 -0700

    connect carousel to article data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,33 @@
 const router = require("express").Router();
+const { Article } = require("../models");
+const { getLinkPreview } = require("link-preview-js");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Article.findAll({
+    attributes: ["id", "title", "author", "article_url"],
+  })
+    .then((dbArticleData) => {
+      const articles = dbArticleData.map((article) =>
+        article.get({ plain: true })
+      );
+
+      return Promise.all(
+        articles.map((article) =>
+          getLinkPreview(article.article_url).then((data) => ({
+            ...article,
+            preview: data,
+          }))
+        )
+      );
+    })
+    .then((articles) => {
+      console.log(articles);
+      res.render("homepage", { articles });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;

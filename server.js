97a39cba05b1b40bdc407a9d49f9e3f20a5d5c02
Lorreina <slore@wood-btw-worlds>
2023-04-10 16:30:46 -0700

    limit carousel to three articles

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection.js");
 const path = require("path");
 const exphbs = require("express-handlebars");
-//const helpers = require("./utils/helpers");
-const hbs = exphbs.create({});
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3002;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,5 @@
-module.exports = {};
+function upto(array, limit) {
+  return array.slice(0, limit);
+}
+
+module.exports = { upto };

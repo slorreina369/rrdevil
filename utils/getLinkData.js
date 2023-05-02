cbc77d0dc539d3c6e9710b208256f015ceea8652
Lorreina <slorreina369@gmail.com>
2023-05-01 17:58:34 -0700

    consolidate default logic

--- utils/getLinkData.js
+++ utils/getLinkData.js
@@ -2,37 +2,37 @@ const { getLinkPreview } = require("link-preview-js");
 const { redisCache } = require("../config/cache");
 
 /**
- *fetches Open Data information for link preview purposes
+ *Fetches Open Data information for link preview purposes
  * @param article
  * @returns Promise
  */
 async function getLinkData(article) {
-  let results;
+  let results = { url: article.article_url };
   try {
     const cacheResults = await redisCache.get(article.article_url);
     if (cacheResults) {
       results = JSON.parse(cacheResults);
-      console.log(results);
     } else {
       results = await getLinkPreview(article.article_url);
-      console.log(results.images);
-      if (results.images.length === 0) {
-        results.images = "/assets/rr_devildefault.jpg";
-        console.log("image replaced");
-      } else {
-        console.log("purely for debug purposes");
-      }
+
       redisCache.set(article.article_url, JSON.stringify(results));
     }
-    return results;
   } catch (error) {
     console.error(`Error: ${error}`);
-    return {
-      url: article.article_url,
-      title: article.title,
-      description: article.description,
-    };
   }
+
+  //Fall back to user submitted data if link preview data isn't present
+  if (!results.title) {
+    results.title = article.title;
+  }
+  if (!results.description) {
+    results.description = article.description;
+  }
+  if (results.images === undefined || results.images.length === 0) {
+    results.images = "/assets/rr_devildefault.jpg";
+  }
+
+  return results;
 }
 
 module.exports = { getLinkData };
